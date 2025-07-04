d0fa7292da2b98150ea9bbdbdfb5159f27e06eb2
EmirhanAk6 <emirhan.akkayaa6@gmail.com>
2025-07-04 14:31:47 +0300

    .

--- src/main/java/com/example/northwind/NorthwindApplication.java
+++ src/main/java/com/example/northwind/NorthwindApplication.java
@@ -4,14 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "My API",
+	        version = "1.0",
+	        description = "Bu API ürünleri yönetir."
+	    )
+	)
+
 
 @SpringBootApplication
-@EnableSwagger2
+
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
@@ -19,12 +28,8 @@ public class NorthwindApplication {
 	}
 	
 	
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("com.example.northwind"))                                       
-          .build();                                           
+	
+                                          
     }
 
-}
+
